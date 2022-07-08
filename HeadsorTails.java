512ec8a635f1f188f555c15b5c4e7b7dbe932257
s1290129 <s1290129@std4dc46>
2022-07-08 12:30:46 +0900

    HoT: initial revision

---
+++ HeadsorTails.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+class HeadsorTails{
+    public static void main(String[] args){
+	Random rand = new Random();
+	int countH, countT;
+	countH = 0;
+	countT = 0;
+	
+ System.out.println("Tossing a coin...");
+ 
+	for(int i = 1; i < 6; i++){
+	    if(rand.nextInt(100)%2 == 0) {
+		System.out.println("Round "+i+": Heads");
+		countH++;
+	    }
+	    else  {
+		System.out.println("Round "+i+": Tails");
+		countT++;
+	    }
+	}
+
+		System.out.println("Heads: "+countH+": Tails: "+countT);
+    }
+}
+	
+	    
